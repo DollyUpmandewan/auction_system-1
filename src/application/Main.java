8dfe473f87698e0af2f3bf90dc5930816e8fedb7
Dheemanth-gowda <dheethu12@gmail.com>
2019-04-03 18:19:35 +0530

    changed the main added main.view to main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,15 +1,38 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+	private Stage primaryStage;
+	private BorderPane mainLayout;
+	
 	@Override
-	public void start(Stage primaryStage) {
-		
-	}
-
+	public void start(Stage primaryStage) throws IOException {
+		this.primaryStage= primaryStage;
+		this.primaryStage.setTitle("Ation System App");
+		showMainView();
+	   }
+   /** 
+ * @throws IOException 
+ * 
+ */
+private void showMainView() throws IOException {
+	   FXMLLoader loader = new FXMLLoader();
+	   loader.setLocation(Main.class.getResource("view/MainView.fxml"));
+	   mainLayout = loader.load();
+	   Scene scene = new Scene(mainLayout);
+	   primaryStage.setScene(scene);
+	   primaryStage.show();
+	   
+	   
+	   
+   }
 	public static void main(String[] args) {
 		launch(args);
 	}
