1c582b98b68a8bdb515366830d0a755c64881080
Dheemanth-gowda <dheethu12@gmail.com.github.com>
2019-04-04 15:53:08 +0530

    Try ti reoair fxml errors

--- src/application/view/MainItemsController.java
+++ src/application/view/MainItemsController.java
@@ -6,6 +6,7 @@ import application.Main;
 import javafx.fxml.FXML;
 
 public class MainItemsController {
+	private Main main;
 	@FXML
 	private void gotobiddingpart() throws IOException {
 		
@@ -17,6 +18,12 @@ public class MainItemsController {
 		
 		Main.showSellerScene();
 		 	
+	}
+	public Main getMain() {
+		return main;
+	}
+	public void setMain(Main main) {
+		this.main = main;
 	}
 
 
