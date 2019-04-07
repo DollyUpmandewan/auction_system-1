4e72691b364e9dd64bc46e738d8861844ba89244
Dheemanth-gowda <dheethu12@gmail.com.github.com>
2019-04-08 01:10:19 +0530

    SignUp page

--- src/application/signup/SignupController.java
+++ src/application/signup/SignupController.java
@@ -1,18 +1,50 @@
 package application.signup;
 
+import java.awt.TextField;
 import java.io.IOException;
+import java.util.Calendar;
 
 import application.Main;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 
 public class SignupController {
+	//This was created fro the main options like to push into the data and take them out 
+	@FXML
+	private TextField nameField;
+	
+	@FXML
+	private TextField PhoneField;
+	
+	@FXML
+	private TextField EmailId;
+	
+	@FXML
+	private TextField Address;
+	
+	// This is for the date of birth and the Age of the user
+	@FXML
+	private DatePicker DateOfBirth;
+	
+	
+	@FXML
+	private TextField agefield;
 	
 	@FXML
 	   private void goHome() throws IOException 
 	   {
 		   Main.showMainItems();
 	   }
-	
+	@FXML
+	private void showAge() {
+		Calendar now = Calendar.getInstance();
+		int year = now.get(Calendar.YEAR);
+		int birthYear = (DateOfBirth.getValue().getYear());
+		int age = year - birthYear;
+		agefield.setText(Integer.toString(age)+ "Years");
+		
+		
+	}
 	@FXML
 	   private void gologinpage() throws IOException 
 	   {
