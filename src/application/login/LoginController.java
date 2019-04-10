5043546f6e01e482543ed4750ef86a897c6d65c2
avinash-rath <avinash.rrath@gmail.com>
2019-04-11 01:09:04 +0530

    Login

--- src/application/login/LoginController.java
+++ src/application/login/LoginController.java
@@ -2,14 +2,13 @@ package application.login;
 
 import java.io.IOException;
 
+import application.LoginAuction;
 import application.Main;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
-
-//TODO add the Fields here just as the ones in the ones in the sign up page.
-//TODO Handle events using the event handlers here. 
+ 
 public class LoginController {
 
     @FXML
@@ -32,12 +31,12 @@ public class LoginController {
 	 @FXML
 	   private void goHome() throws IOException 
 	   {
-		 //TODO : Bro here we need to add the code to retrive the data from database and check it with the loginid and loginpass 
-		 //to check if they match 
-		 //If its true call the Main.showMainItems function as below and if not then ak to sign up 
-		 //IE: Call the signUp page function
-		 
-		   Main.showMainItems();
+		 	String logID = loginId.getText();
+		 	String logPass = loginpass.getText();
+		 	LoginAuction la = new LoginAuction();
+		 	if(la.LoginCheck(logID,logPass))
+		 		Main.showMainItems();
+		   
 	   }
 	
 	 
