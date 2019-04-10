c8e71ca714d89c517d5a055d842b77218330bd43
Dheemanth-gowda <dheethu12@gmail.com.github.com>
2019-04-11 03:21:48 +0530

    Signup page added

---
+++ src/application/payment/PaymentController.java
@@ -0,0 +1,5 @@
+package application.payment;
+
+public class PaymentController {
+
+}
--- src/application/signup/SignupController.java
+++ src/application/signup/SignupController.java
@@ -4,33 +4,50 @@ import java.awt.TextField;
 import java.io.IOException;
 import java.util.Calendar;
 
+import application.LoginAuction;
 import application.Main;
+import application.SignUp;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 
 public class SignupController {
 	public int age;
 	//This was created from the main options like to push into the data and take them out 
 	// Seen(by avinash);
-	@FXML
-	private TextField nameField;
-	
-	@FXML
-	private TextField PhoneField;
-	
-	@FXML
-	private TextField EmailId;
-	
-	@FXML
-	private TextField Address;
-	
-	// This is for the date of birth and the Age of the user
-	@FXML
-	private DatePicker DateOfBirth;
-	
-	
-	@FXML
-	private TextField agefield;
+	 @FXML
+	    private Button signupbtn;
+
+	    @FXML
+	    private TextField FnameField;
+
+	    @FXML
+	    private TextField PhoneField;
+
+	    @FXML
+	    private TextField EmailId;
+
+	    @FXML
+	    private TextField Addressfield;
+	    
+
+	    @FXML
+	    private TextField userfield;
+
+	    @FXML
+	    private TextField passonefield;
+
+	    @FXML
+	    private TextField passtwofield;
+
+	    @FXML
+	    private TextField LnameField;
+
+	    @FXML
+	    public DatePicker DateOfBirth;
+
+	    @FXML
+	    private TextField agefield;
 	
 	@FXML
 	   private void goHome() throws IOException 
@@ -50,7 +67,21 @@ public class SignupController {
 	@FXML
 	   private void gologinpage() throws IOException 
 	   {
+		
 		   Main.showLoginPage();
+//String Fname, String Lname, String username, String password,
+//String password2, String address,String mobile, String DoB, int userType
+		   String Fname = FnameField.getText();
+		   String Lname = LnameField.getText();
+		   String username = userfield.getText();
+		   String password = passonefield.getText();
+		   String password2 = passtwofield.getText();
+		   String address = Addressfield.getText();
+		   String mobile = PhoneField.getText();
+		  // Double DoB = DateOfBirth.getText();
+		   SignUp ca = new SignUp();
+		   
+		   
 	   }
 	
 
